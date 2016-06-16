13d7e6f6c21898668d46aec274227f1cad7664ca
MuhunKim <yale2a11@gmail.com>
2016-06-16 20:54:25 +0900

    Add semantic icon javascript code

--- js/ui.js
+++ js/ui.js
@@ -6,3 +6,15 @@ function openSide(){
 $(document).ready(function(){
   $('.tabular.menu a.item').tab();
 });
+
+// semantic icon
+semantic.icon = {};
+// 이벤트 시작
+semantic.icon.ready = function() {
+  var $grid = $('.ui.five.column.doubling.grid'),
+  handler;
+};
+// attach ready event
+$(document)
+  .ready(semantic.icon.ready)
+;
